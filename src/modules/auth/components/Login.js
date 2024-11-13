a64782b05170d8fe91016165a829ca674aa16ed1
jisusin <130023628+jisusin@users.noreply.github.com>
2024-11-13 16:20:04 +0900

    User Authentication Design Integration #9 (#13)
    - Merge Login.module.css and SignUp.module.css into auth.module.css.
    - Update style references in Login.js and SignUp.js to use auth.module.css.
    - Delete Login.module.css and SignUp.module.css.

--- src/modules/auth/components/Login.js
+++ src/modules/auth/components/Login.js
@@ -1,7 +1,7 @@
 // components/Login.js
 import React from 'react';
 import useLogin from '../hooks/useLogin';  // 훅을 가져옵니다.
-import styles from './Login.module.css';
+import styles from './auth.module.css';
 
 const Login = () => {
     const { inputKey, message, setInputKey, handleLogin } = useLogin();  // 훅에서 상태와 함수 가져오기
@@ -10,37 +10,41 @@ const Login = () => {
     const loggedIn = localStorage.getItem('loginState');
     return (
         <>
-        <div className={styles.loginContainer}>
+        <div className={styles.container}>
         <h2>Matter Tunnel</h2>
-            {(loggedIn) && (<div>
-                현재 로그인 중인 개인키 <br />
-                {loggedInKey}
-            </div>)}
-            {(<div className={styles.formContainer}>
-                <input 
-                    type="text"
-                    placeholder="개인 키를 입력하세요"
-                    value={inputKey}
-                    onChange={(e) => setInputKey(e.target.value)}  // 입력값 변경 처리
-                    className={styles.inputField}
-                />
-                <button 
-                    onClick={handleLogin}  // 로그인 버튼 클릭 시 처리
-                    className={styles.loginButton}
-                >
-                    로그인
-                </button>
-                
-                {message && (
-                    <p className={`${styles.message} ${
-                        message === 'Login Completed' 
-                            ? styles.successMessage 
-                            : styles.errorMessage
-                    }`}>
-                        {message}
-                    </p>
+            <div className={styles.loginContainer}>
+                <span className={styles.keyLabel}>현재 로그인 중인 개인키</span>
+                {(loggedIn) && (
+                    <div className={styles.loggedInInfo}>
+                        {loggedInKey}
+                    </div>
                 )}
-            </div>)}
+                {(<div className={styles.formContainer}>
+                    <input 
+                        type="text"
+                        placeholder="개인 키를 입력하세요"
+                        value={inputKey}
+                        onChange={(e) => setInputKey(e.target.value)}  // 입력값 변경 처리
+                        className={styles.inputField}
+                    />
+                    <button 
+                        onClick={handleLogin}  // 로그인 버튼 클릭 시 처리
+                        className={styles.loginButton}
+                    >
+                        로그인
+                    </button>
+                    
+                    {message && (
+                        <p className={`${styles.message} ${
+                            message === 'Login Completed' 
+                                ? styles.successMessage 
+                                : styles.errorMessage
+                        }`}>
+                            {message}
+                        </p>
+                    )}
+                </div>)}
+            </div>
         </div>
         </>
     );
