8dcb749c8caa51a1fbbd91c909a47f812f6e55b3
jisusin <130023628+jisusin@users.noreply.github.com>
2024-11-22 07:49:10 +0900

    Modify auth.module.css
    - Modify auth.module.css

--- src/modules/auth/components/Login.js
+++ src/modules/auth/components/Login.js
@@ -1,14 +1,13 @@
 // components/Login.js
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import useLogin from '../hooks/useLogin';  // 훅을 가져옵니다.
+import useLogin from '../hooks/useLogin';
 import styles from './auth.module.css';
 
 const Login = () => {
     const navigate = useNavigate();
-    const { inputKey, message, setInputKey, handleLogin } = useLogin();  // 훅에서 상태와 함수 가져오기
+    const { inputKey, message, setInputKey, handleLogin } = useLogin();
     const loggedInKey = localStorage.getItem('privateKey');
-    console.log(loggedInKey);
     const loggedIn = localStorage.getItem('loginState');
 
     useEffect(() => {
@@ -16,48 +15,48 @@ const Login = () => {
             navigate('/device');
         }
     }, [navigate, loggedIn]);
-    
+
     return (
-        <>
-        <h2>Matter Tunnel</h2>
-        <div className={styles.container}>
-            <div className={styles.loginContainer}>
-                {loggedIn && (
-                    <>
-                        <span className={styles.keyLabel}>현재 로그인 중인 개인키</span>
-                        <div className={styles.loggedInInfo}>
-                            {loggedInKey}
-                        </div>
-                    </>
-                )}
-                {(<div className={styles.formContainer}>
-                    <input 
-                        type="text"
-                        placeholder="개인 키를 입력하세요"
-                        value={inputKey}
-                        onChange={(e) => setInputKey(e.target.value)}  // 입력값 변경 처리
-                        className={styles.inputField}
-                    />
-                    <button 
-                        onClick={handleLogin}  // 로그인 버튼 클릭 시 처리
-                        className={styles.loginButton}
-                    >
-                        로그인
-                    </button>
-                    
-                    {message && (
-                        <p className={`${styles.message} ${
-                            message === 'Login Completed' 
-                                ? styles.successMessage 
-                                : styles.errorMessage
-                        }`}>
-                            {message}
-                        </p>
+        <div className={styles.pageContainer}>
+            <h2>Matter Tunnel</h2>
+            <div className={styles.container}>
+                <div className={styles.loginContainer}>
+                    {loggedIn && (
+                        <>
+                            <span className={styles.keyLabel}>현재 로그인 중인 개인키</span>
+                            <div className={styles.loggedInInfo}>
+                                {loggedInKey}
+                            </div>
+                        </>
                     )}
-                </div>)}
+                    {(<div className={styles.formContainer}>
+                        <input 
+                            type="text"
+                            placeholder="개인 키를 입력하세요"
+                            value={inputKey}
+                            onChange={(e) => setInputKey(e.target.value)}
+                            className={styles.inputField}
+                        />
+                        <button
+                            onClick={handleLogin}
+                            className={styles.loginButton}
+                        >
+                            로그인
+                        </button>
+                        
+                        {message && (
+                            <p className={`${styles.message} ${
+                                message === 'Login Completed'
+                                    ? styles.successMessage
+                                    : styles.errorMessage
+                            }`}>
+                                {message}
+                            </p>
+                        )}
+                    </div>)}
+                </div>
             </div>
         </div>
-        </>
     );
 };
 
